313738f57622b812ef29f21273c545f7196921c9
Mdleal <michael.leal82@gmail.com>
2014-12-10 09:58:41 -0600

    edit local

--- assets/php/service.class.php
+++ assets/php/service.class.php
@@ -7,7 +7,7 @@ class service
 	public $host;
 	public $status;
 	
-	function __construct($name, $port, $url = "", $host = "localhost")
+	function __construct($name, $port, $url = "", $host = "")
 	{
 		$this->name = $name;
 		$this->port = $port;
