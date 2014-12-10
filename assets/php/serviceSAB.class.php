0f3312a232d9035714755bd12c4ff820cda6b92c
Mdleal <michael.leal82@gmail.com>
2014-12-10 09:32:10 -0600

    updating

--- assets/php/serviceSAB.class.php
+++ assets/php/serviceSAB.class.php
@@ -32,7 +32,8 @@ class serviceSAB
 	function makeButton()
 	{
 		global $sabnzbd_api;
-		$sabnzbdXML = simplexml_load_file('http://127.0.0.1:7878/api?mode=qstatus&output=xml&apikey='.$sabnzbd_api);
+		global $sab_server_ip;
+		$sabnzbdXML = simplexml_load_file('http://'.$sab_server_ip.'/api?mode=qstatus&output=xml&apikey='.$sabnzbd_api);
 
 		if (($sabnzbdXML->state) == 'Downloading'):
 			$icon = '<i class="icon-' . ($this->status ? 'download-alt' : 'remove') . ' icon-white"></i>';
