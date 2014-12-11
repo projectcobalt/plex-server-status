9ed7f2a0ad90b89dc77257533b7f289d867970fd
Mdleal <michael.leal82@gmail.com>
2014-12-11 08:23:33 -0600

    updating

--- assets/php/ping_ajax.php
+++ assets/php/ping_ajax.php
@@ -1,15 +1,16 @@
-//<!DOCTYPE html>
-//<?php
-//	Ini_Set( 'display_errors', true );
-//	include("functions.php");
-//?>
-//<html lang="en">
-//	<script>
-//		// Enable bootstrap tooltips
-//		$(function ()
-//		        { $("[rel=tooltip]").tooltip();
-//		        });
-//	</script>
-//<?php
-//echo ping();
-//?>
+<!DOCTYPE html>
+<?php
+	Ini_Set( 'display_errors', true );
+	include '../../init.php';
+	include ROOT_DIR . '/assets/php/functions.php';
+?>
+<html lang="en">
+	<script>
+		// Enable bootstrap tooltips
+		$(function ()
+		        { $("[rel=tooltip]").tooltip();
+		        });
+	</script>
+<?php
+echo ping();
+?>
