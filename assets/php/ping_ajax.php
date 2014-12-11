395be776e20878a2f27c6e9bfeab85563da5f1ad
Mdleal <michael.leal82@gmail.com>
2014-12-11 10:54:09 -0600

    Update ping_ajax.php

--- assets/php/ping_ajax.php
+++ assets/php/ping_ajax.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
 	Ini_Set( 'display_errors', true );
-	include '../../init.php';
+	//include '../../init.php';
 	include ROOT_DIR . '/assets/php/functions.php';
 ?>
 <html lang="en">
