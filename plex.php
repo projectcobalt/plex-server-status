982eaf2b3e67df79e90404f85e858328b57efafd
Mdleal <michael.leal82@gmail.com>
2014-12-11 15:17:51 -0600

    agjusting image

--- plex.php
+++ plex.php
@@ -1,16 +1,10 @@
 <?php
-include 'init.php';
+//include 'init.php';
 include ROOT_DIR . '/assets/php/functions.php';
 $image_url = $_GET['img'];
-$network = getNetwork();
-$plexAddress = $plex_server_ip;
-$addressPosition = strpos($image_url, $plexAddress);
-if($addressPosition !== false && $addressPosition == 0) {
-	$image_src = $image_url . '?X-Plex-Token=' . $plexToken;
-	header('Content-type: image/jpeg');
-	//header("Content-Length: " . filesize($image_src));
-	readfile($image_src);
-} else {
-echo "Bad Plex Image Url";	
-}
+$image_src = $image_url . '?X-Plex-Token=' . $plexToken;
+header('Content-type: image/jpeg');
+//header("Content-Length: " . filesize($image_src));
+readfile($image_src);
+
 ?>
