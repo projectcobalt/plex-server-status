e2f08d13d12bbb7a510567ef3c0409153a7e1f9a
Mdleal <michael.leal82@gmail.com>
2014-12-11 08:33:45 -0600

    edit php

--- assets/php/plex_check_ajax.php
+++ assets/php/plex_check_ajax.php
@@ -1,27 +1,42 @@
-//<!DOCTYPE html>
-//<?php
-//	Ini_Set( 'display_errors', true );
-//	include("functions.php");
-//
-//	$plexSessionXML = simplexml_load_file($plex_server_ip.'/status/sessions');
-//	$plexcheckfile1 = '../misc/plexcheckfile1.txt';
-//	$plexcheckfile2 = '../misc/plexcheckfile2.txt';
-//	$plexcheckfile1_md5 = md5_file($plexcheckfile1);
-//	$plexcheckfile2_md5 = md5_file($plexcheckfile2);
-//	$nowplaying = false;
-//	$viewers = 0;
-//
-//	if (count($plexSessionXML->Video) > 0) {
-//		$viewers = count($plexSessionXML->Video);
-//	}
-//	file_put_contents($plexcheckfile1, $viewers, LOCK_EX);
-//
-//	
-//	if ($plexcheckfile1_md5 === $plexcheckfile2_md5) {
-//		// if they are the same do nothing
-//	} else {
-//		// If they are different, update plexcheckfile2
-//		file_put_contents($plexcheckfile2, $viewers, LOCK_EX);
-//	}
-//	
-//?>
+<?php
+	Ini_Set( 'display_errors', true );
+	include '../../init.php';
+	include 'functions.php';
+
+	$plexSessionXML = simplexml_load_file('http://'.$plex_server_ip.':'.$plex_port.'/status/sessions');
+	$plexcheckfile1 = ROOT_DIR . '/assets/caches/plexcheckfile1.txt';
+	$plexcheckfile2 = ROOT_DIR . '/assets/caches/plexcheckfile2.txt';
+	$plexcheckfile1_md5 = md5_file($plexcheckfile1);
+	$plexcheckfile2_md5 = md5_file($plexcheckfile2);
+	$viewers = 0;
+
+	// See if Plex Media Server is online and how many people are watching.
+	if (!$plexSessionXML):
+		// If Plex Media Server is offline.
+		$plexStatus = 'offline';
+	else:
+		// If Plex Media Server is online.
+		$plexStatus = 'online';
+		// Count how many people are watching.
+		if (count($plexSessionXML->Video) > 0) {
+			$viewers = count($plexSessionXML->Video);
+		}
+	endif;
+
+	// Build an array to hold the values
+	$array = [
+		'status' => $plexStatus,
+		'viewers' => $viewers,
+	];
+
+	// Write the data out to the first Plex check file
+	file_put_contents($plexcheckfile1, $array, LOCK_EX);
+	
+	// Check to see if it's the same as the second Plex check file
+	if ($plexcheckfile1_md5 === $plexcheckfile2_md5) {
+		// if they are the same do nothing
+	} else {
+		// If they are different, update plexcheckfile2
+		file_put_contents($plexcheckfile2, $array, LOCK_EX);
+	}
+?>
