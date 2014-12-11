3053dae41796d661edbfcfc3c441cd593b1e61db
Mdleal <michael.leal82@gmail.com>
2014-12-11 15:33:50 -0600

    adding global

--- plex.php
+++ plex.php
@@ -1,6 +1,8 @@
 <?php
 //include 'init.php';
 include ROOT_DIR . '/assets/php/functions.php';
+global $plexToken;
+
 $image_url = $_GET['img'];
 $image_src = $image_url . '?X-Plex-Token=' . $plexToken;
 header('Content-type: image/jpeg');
