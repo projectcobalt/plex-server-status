1b328e40085c18877a7fd88c2e7b1069e45ee760
Mdleal <michael.leal82@gmail.com>
2014-12-11 12:52:52 -0600

    Update plex.php

--- plex.php
+++ plex.php
@@ -3,7 +3,7 @@ include 'init.php';
 include ROOT_DIR . '/assets/php/functions.php';
 $image_url = $_GET['img'];
 $network = getNetwork();
-$plexAddress = $network.':'.$plex_port;
+$plexAddress = $plex_server_ip.':'.$plex_port;
 $addressPosition = strpos($image_url, $plexAddress);
 if($addressPosition !== false && $addressPosition == 0) {
 	$image_src = $image_url . '?X-Plex-Token=' . $plexToken;
